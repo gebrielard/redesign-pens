e6fff0cba967031fcf83f9250e5a177f7885edce
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-06-30 08:04:40 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $(function () {
+//   var nav = document.getElementById("mainNav");
+//   var topnav = document.getElementsById("topNav").height();
+
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > topnav) {
+//       nav.addClass("sticky");
+//     } else {
+//       nav.removeClass("sticky");
+//     }
+//   });
+// });
